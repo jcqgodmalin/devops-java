b0c9eebd7914cef36bcb0b68dc1be62d93fffb27
Godmalin, Jhon <jhon.c.godmalin@accenture.com>
2021-10-17 09:42:04 +0800

    added cors origin policy

--- src/main/java/com/accenture/ws/controller/OrderAndBillingController.java
+++ src/main/java/com/accenture/ws/controller/OrderAndBillingController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.accenture.ws.repository.OrderRepository;
 @RestController
 @RequestMapping("/api")
 @Transactional
+@CrossOrigin(origins = "http://localhost:8080")
 public class OrderAndBillingController {
 
 	
